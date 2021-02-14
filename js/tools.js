13c9bb6295f36044c54b972a1beecfd0c552584a
Felix Berger <felix.berger98@icloud.com>
2021-02-14 15:16:36 +0100

    Tools erweitert

--- js/tools.js
+++ js/tools.js
@@ -1,26 +1,50 @@
-function calculateDateDifference()
-{
-  var startDate = new Date(document.getElementsByName("from")[0].value);
-  var endDate = new Date(document.getElementsByName("to")[0].value);
-  var result = document.getElementById("result");
-  result.innerHTML = "";
-  if(startDate == "Invalid Date" || endDate == "Invalid Date"){
-      result.innerHTML = "Bitte beide Felder ausfüllen."
-  }
-  else
-  {
-  var diff = Math.abs(endDate - startDate);
-  var years = Math.abs(Math.floor((diff / (31536000000))));
-  diff = diff - (years * 31536000000);
-  var days = Math.abs(Math.floor((diff / (86400000))));
-  diff = diff - (days * 86400000);
-  var hours = Math.abs(Math.floor((diff / (3600000))));
-  diff = diff - (hours * 3600000);
-  var minutes = Math.abs(Math.floor((diff / (60000))));
-  result.innerHTML += "Der Unterschied  liegt bei: <br>";
-  result.innerHTML += years + " Jahren,<br>";
-  result.innerHTML += days + " Tagen,<br>";
-  result.innerHTML += hours + " Stunden,<br>";
-  result.innerHTML += minutes + " Minuten.<br>.";
-  }
+function calculateDateDifference() {
+    //Empty elements
+    document.getElementsByClassName("result")[0] == "";
+    document.getElementsByClassName("result")[1] == "";
+
+    var startDate = new Date(document.getElementsByName("from")[0].value);
+    var endDate = new Date(document.getElementsByName("to")[0].value);
+    var result = document.getElementsByClassName("result")[0];
+
+    result.innerHTML = "";
+
+    //Check for input
+    if (startDate == "Invalid Date" || endDate == "Invalid Date") {
+        result.innerHTML = "Bitte beide Felder ausfüllen."
+    } else {
+        var diff = Math.abs(endDate - startDate);
+        var years = Math.abs(Math.floor((diff / (31536000000))));
+        diff = diff - (years * 31536000000);
+        var days = Math.abs(Math.floor((diff / (86400000))));
+        diff = diff - (days * 86400000);
+        var hours = Math.abs(Math.floor((diff / (3600000))));
+        diff = diff - (hours * 3600000);
+        var minutes = Math.abs(Math.floor((diff / (60000))));
+        result.innerHTML += "Der Unterschied  liegt bei: <br>";
+        result.innerHTML += years + " Jahren,<br>";
+        result.innerHTML += days + " Tagen,<br>";
+        result.innerHTML += hours + " Stunden,<br>";
+        result.innerHTML += minutes + " Minuten.<br>.";
+    }
+}
+
+function calculateWorkEnd() {
+    //Empty Elements
+    document.getElementsByClassName("result")[0] == "";
+    document.getElementsByClassName("result")[1] == "";
+
+    var startDate = new Date(document.getElementsByName("start")[0].value);
+    var endDate = null;
+    var result = document.getElementsByClassName("result")[1];
+
+    result.innerHTML = "";
+
+    //Check for input
+    if (startDate == "Invalid Date") {
+        result.innerHTML = "Bitte Feld ausfüllen."
+    } else {
+        endDate = new Date(startDate.getTime() + (8 * 60 * 60 * 1000 + 1000 * 60 * 30));
+        result.innerHTML += "Nach 8:30 Arbeitszeit, kannst du um " + endDate.getHours() + ":" + endDate.getMinutes() + " Uhr Feierabend machen";
+    }
 }
